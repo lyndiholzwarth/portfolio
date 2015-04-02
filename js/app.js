f02e32570c97a79f3cd0ca9da276aee4fe7bd49e
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-04-01 20:18:37 -0400

    footer issue

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+//	$('#digital').mouseenter(function(){
+//		$('.content').hide();
+//		$('.digital-img').show();
+//	})
+//	.mouseleave(function(){
+//		$('.content').show();
+//		$('.digital-img').hide();
+//	});
+//
+//	$('#print').mouseenter(function(){
+//		$('.content').hide();
+//		$('print-img').show();
+//	})
+//	.mouseleave(function(){
+//		$('.content').show();
+//		$('.print-img').hide();
+//	});
+
+
+
+});
