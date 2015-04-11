4f511ddffc3780a72c3227700a918a937b92122c
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-04-11 14:45:26 -0400

    scroller working

--- js/app.js
+++ js/app.js
@@ -3,11 +3,46 @@ $(document).ready(function(){
 
 	$('.hover-area').mouseenter(function(){
 		$(this).fadeTo( "slow", 0.3 );
-		$('.skills').fadeTo("slow", 1);
+		$('.skills', this).fadeTo("slow", 1);
 	});
 
 	$('.hover-area').mouseleave(function(){
 		$(this).fadeTo("slow", 1);
 		$('.skills').fadeTo("slow", 0);
 	});
+
+	$(function(){setInterval('rotateImgAd()', 3000);
+	});
+
+	$(function(){setInterval('rotateImgPoster()', 3000);
+	});
+
 });
+
+	function rotateImgAd(){
+		var curAdImg = $('#ad-scroll div.current-a');
+		var nxtAdImg = curAdImg.next();
+		if(nxtAdImg.length==0)
+			nxtAdImg = $('#ad-scroll div:first');
+
+		curAdImg.removeClass('current-a').addClass('previous-a');
+		nxtAdImg.addClass('current-a').animate({opacity: 1.0}, 1000,
+			function(){
+				curAdImg.removeClass('previous-a');
+			});
+
+	};
+
+	function rotateImgPoster(){
+		var curPostImg = $('#poster-scroll div.current-p');
+		var nxtPostImg = curPostImg.next();
+		if(nxtPostImg.length==0)
+			nxtPostImg = $('#poster-scroll div:first');
+
+		curPostImg.removeClass('current-p').addClass('previous-p');
+		nxtPostImg.addClass('current-p').animate({opacity: 1.0}, 1000,
+			function(){
+				curPostImg.removeClass('previous-p');
+			});
+
+	};	
