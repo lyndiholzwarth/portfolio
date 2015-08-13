d7b78ebea5fe4b76e8e844d4704fe2719769fd1a
Lyndi Holzwarth <lyndi.holzwarth@gmail.com>
2015-08-13 17:02:20 -0400

    fixed sticky scroll

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	//sticky header, with size adjustment
 	$(window).scroll(function() {
-	if ($(this).scrollTop() > 664){  
+	if ($(this).scrollTop() > 653){  
 	    $('.navbar').addClass("navbar-fixed-top");
 	    $('.about').addClass("fixed-nav");
 	  }
